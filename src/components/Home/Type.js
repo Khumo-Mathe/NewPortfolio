9f76bd759834b78ef513974d9b85992fd3ca63e9
Khumo Mathe <92854875+Khumo-Mathe@users.noreply.github.com>
2026-01-31 17:52:58 +0200

    Update job titles in Type.js component

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -8,8 +8,8 @@ function Type() {
         strings: [
           "Software Developer",
           "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Systems engineer",
+          "IT Consultant",
         ],
         autoStart: true,
         loop: true,
